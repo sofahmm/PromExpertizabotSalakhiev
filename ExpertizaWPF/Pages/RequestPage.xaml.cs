459aa9566a721d9b1c8b580196bc326916280786
Sofia <ASUS@LAPTOP-DSSEENGE>
2025-06-12 12:16:00 +0300

    фиксирую

--- ExpertizaWPF/Pages/RequestPage.xaml.cs
+++ ExpertizaWPF/Pages/RequestPage.xaml.cs
@@ -30,10 +30,63 @@ namespace ExpertizaWPF.Pages
             this.DataContext = this;
             //LoadRequests();
         }
-        //private void LoadRequests()
-        //{
-        //    var requests = 
-        //    RequestsLv.ItemsSource = requests;  
-        //}
+
+        private void StatusReqCmb_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var t = (StatusReqCmb.SelectedItem as ComboBoxItem)?.Content.ToString();
+            if (t == "Новые")
+                RequestsLv.ItemsSource = dbService.GetAllAplications().Where(i => i.Status == "Новая").ToList();
+            else if (t == "Закрытые")
+                RequestsLv.ItemsSource = dbService.GetAllAplications().Where(i => i.Status == "Закрыта").ToList();
+            else if (t == "Показать всё")
+                RequestsLv.ItemsSource = dbService.GetAllAplications();
+        }
+
+        private void ObjectCmb_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var t = (ObjectCmb.SelectedItem as ComboBoxItem)?.Content.ToString();
+            if (t == "Один")
+                RequestsLv.ItemsSource = dbService.GetAllAplications().Where(i => i.Object_Id == "один").ToList();
+            else if (t == "Два")
+                RequestsLv.ItemsSource = dbService.GetAllAplications().Where(i => i.Object_Id == "два").ToList();
+            else if (t == "Три")
+                RequestsLv.ItemsSource = dbService.GetAllAplications().Where(i => i.Object_Id == "три").ToList();
+            else if (t == "Четыре")
+                RequestsLv.ItemsSource = dbService.GetAllAplications().Where(i => i.Object_Id == "четыре").ToList();
+            else if (t == "Пять")
+                RequestsLv.ItemsSource = dbService.GetAllAplications().Where(i => i.Object_Id == "пять").ToList();
+            else if (t == "Два")
+                RequestsLv.ItemsSource = dbService.GetAllAplications().Where(i => i.Object_Id == "шесть").ToList();
+            else if (t == "Показать всё")
+                RequestsLv.ItemsSource = dbService.GetAllAplications();
+        }
+
+        private void SortDateCmb_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var t = (SortDateCmb.SelectedItem as ComboBoxItem)?.Content.ToString();
+            if (t == "По дате(по возрастанию)")
+                RequestsLv.ItemsSource = dbService.GetAllAplications().OrderBy(i => i.Date_create);
+            else if (t == "По дате(по убыванию)")
+                RequestsLv.ItemsSource = dbService.GetAllAplications().OrderByDescending(i => i.Date_create);
+            else if (t == "Показать всё")
+                RequestsLv.ItemsSource = dbService.GetAllAplications();
+        }
+
+        private void SearchExecuterTb_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if(SearchExecuterTb.Text != "")
+                RequestsLv.ItemsSource = dbService.GetAllAplications().Where(i => i.Description != null && i.Description.ToLower()
+                .Contains(SearchExecuterTb.Text.Trim().ToLower())).ToList();
+            else
+                RequestsLv.ItemsSource = dbService.GetAllAplications();
+        }
+
+        private void RequestsLv_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var t = RequestsLv.SelectedItem as DataBase.Application;
+            Windows.AboutRequestWindow aboutRequestWindow = new Windows.AboutRequestWindow(t);
+            aboutRequestWindow.Show();
+
+        }
     }
 }
---
+++ ExpertizaWPF/Windows/AboutRequestWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ExpertizaWPF.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для AboutRequestWindow.xaml
+    /// </summary>
+    public partial class AboutRequestWindow : Window
+    {
+        public static DataBase.Application application1 = new DataBase.Application();
+        public AboutRequestWindow(DataBase.Application application)
+        {
+            InitializeComponent();
+            application1 = application;
+
+            UserNameTbl.Text = application1.User;
+            if(application1.Status == "Закрыта")
+            {
+                StatusTbl.Foreground = Brushes.Green;
+            }
+            else
+            {
+                StatusTbl.Foreground = Brushes.Red;
+            }
+            StatusTbl.Text = application1.Status;
+            IdRequestTbl.Text = application1.Id.ToString();
+            ExecutorNameTbl.Text = application1.Executor;
+            DescriptionTbl.Text = application1.Description; 
+            DateCreateTbl.Text = application1.Date_create;
+            DateCloseTbl.Text = application1.Date_close;
+        }
+    }
+}
